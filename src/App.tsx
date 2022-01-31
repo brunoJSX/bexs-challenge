ccec57723d0da6212d46d27bad9a84fba0417a3d
Bruno Henrique <bruno28dpvat@gmail.com>
2022-01-31 17:59:08 -0400

    refactor: added code to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { CreditCard, ICardExpiration } from './components/CreditCard';
 
 import GlobalStyle from './styles/global';
 
 function App() {
-  return <GlobalStyle />;
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiration, setExpiration] = useState<ICardExpiration>({
+    month: 12,
+    year: 50,
+  });
+
+  return (
+    <>
+      <GlobalStyle />
+
+      <CreditCard
+        cardNumber={number}
+        personName={name}
+        cardExpiration={expiration}
+      />
+
+      <label>Number</label>
+      <input
+        type="text"
+        value={number}
+        onChange={e => setNumber(e.target.value)}
+      />
+
+      <label>Name</label>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+
+      {/* <label>Expiration</label>
+      <input
+        type="text"
+        value={`${expiration.month}/${expiration.year}`}
+        onChange={handleExpirationDate}
+      /> */}
+    </>
+  );
 }
 
 export default App;
