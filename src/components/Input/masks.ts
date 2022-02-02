9b925e08304feb9bd15bdd06664d2d77593e7a3d
Bruno Henrique <bruno28dpvat@gmail.com>
2022-02-02 15:01:59 -0400

    fix: fixed creditCardExpitationDate mask

--- src/components/Input/masks.ts
+++ src/components/Input/masks.ts
@@ -18,12 +18,14 @@ function creditCardNumber(e: FormEvent<HTMLInputElement>) {
 function creditCardExpirationDate(e: FormEvent<HTMLInputElement>) {
   e.currentTarget.maxLength = 5;
 
-  const { value } = e.currentTarget;
-
-  e.currentTarget.value = value
-    .replace(/\D/g, '')
-    .replace(/(\d{2})/g, '$1/')
-    .trim();
+  e.currentTarget.value = e.currentTarget.value
+    .replace(/^([1-9]\/|[2-9])$/g, '0$1/')
+    .replace(/^(0[1-9]|1[0-2])$/g, '$1/')
+    .replace(/^([0-1])([3-9])$/g, '0$1/$2')
+    .replace(/^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2')
+    .replace(/^([0]+)\/|[0]+$/g, '0')
+    .replace(/[^\d/]|^[/]*$/g, '')
+    .replace(/\/\//g, '/');
 
   return e;
 }
