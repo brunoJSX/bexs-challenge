81d2edb449935c93a0f87d3ae64203b35ca7c3e6
Bruno Henrique <bruno28dpvat@gmail.com>
2022-02-03 13:43:26 -0400

    fix: fixed localhost url on api config

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'test',
+    process.env.NODE_ENV !== 'production' ? 'http://localhost:3333' : 'test',
 });
 
 export default api;
